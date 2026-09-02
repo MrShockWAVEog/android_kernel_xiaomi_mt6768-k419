lospolloshermanosdev/kernel-xiaomi-mt6768-S

drivers: mediatek: Import Xiaomi cam_cal changes

drivers/misc/mediatek/cam_cal/src/mt6768/cam_cal_list.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2018 MediaTek Inc.
+ * Copyright (C) 2021 XiaoMi, Inc.
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License version 2 as
@@ -15,12 +16,60 @@
 #include "eeprom_i2c_common_driver.h"
 #include "eeprom_i2c_custom_driver.h"
 #include "kd_imgsensor.h"
+#ifdef GC02M1_MIPI_RAW
+//extern gc02m1_read_otp_info();
+extern unsigned int gc02m1_read_otp_info(struct i2c_client *client,
+	unsigned int addr,
+	unsigned char *data,
+	unsigned int size);
+#endif
+#ifdef GC02M1_SUNNY_MIPI_RAW
+extern unsigned int gc02m1_sunny_read_otp_info(struct i2c_client *client,
+	unsigned int addr,
+	unsigned char *data,
+	unsigned int size);
+#endif
 
+#ifdef CONFIG_TARGET_PRODUCT_LANCELOTCOMMON
+#if defined(OV8856_QTECH_FRONT_MIPI_RAW)
+extern unsigned int ov8856_qtech_front_read_otp_info(struct i2c_client *client, unsigned int addr, unsigned char *data, unsigned int size);
+#endif
+#endif
 
-
+#ifdef CONFIG_TARGET_PRODUCT_SHIVACOMMON
+#if defined(OV8856_QTECH_FRONT_MIPI_RAW)
+extern unsigned int ov8856_qtech_front_read_otp_info(struct i2c_client *client, unsigned int addr, unsigned char *data, unsigned int size);
+#endif
+#endif
 
 struct stCAM_CAL_LIST_STRUCT g_camCalList[] = {
 	/*Below is commom sensor */
+#ifdef CONFIG_TARGET_PRODUCT_LANCELOTCOMMON
+	{OV13B10_OFILM_SENSOR_ID, 0xA2, Common_read_region},
+	{OV13B10_QTECH_SENSOR_ID, 0xA2, Common_read_region},
+	{S5K3L6_QTECH_SENSOR_ID, 0xA2, Common_read_region},
+	{S5K4H7YX_OFILM_FRONT_SENSOR_ID, 0xA0, Common_read_region},
+	{S5K4H7YX_OFILM_ULTRA_SENSOR_ID, 0xA8, Common_read_region},
+	{S5K4H7YX_QTECH_FRONT_SENSOR_ID, 0x20, Common_read_region},
+	{S5K4H7YX_QTECH_ULTRA_SENSOR_ID, 0x5A, Common_read_region},
+	{OV8856_QTECH_ULTRA_SENSOR_ID, 0xA8, Common_read_region},
+	{OV8856_QTECH_FRONT_SENSOR_ID, 0x6C, ov8856_qtech_front_read_otp_info},
+	{GC5035_OFILM_SENSOR_ID, 0xA4, Common_read_region},
+	{GC5035_QTECH_SENSOR_ID, 0xA4, Common_read_region},
+#endif
+#ifdef CONFIG_TARGET_PRODUCT_SHIVACOMMON
+	{OV13B10_OFILM_SENSOR_ID, 0xA2, Common_read_region},
+	{OV13B10_QTECH_SENSOR_ID, 0xA2, Common_read_region},
+	{S5K3L6_QTECH_SENSOR_ID, 0xA2, Common_read_region},
+	{S5K4H7YX_OFILM_FRONT_SENSOR_ID, 0xA0, Common_read_region},
+	{S5K4H7YX_OFILM_ULTRA_SENSOR_ID, 0xA8, Common_read_region},
+	{S5K4H7YX_QTECH_FRONT_SENSOR_ID, 0x20, Common_read_region},
+	{S5K4H7YX_QTECH_ULTRA_SENSOR_ID, 0x5A, Common_read_region},
+	{OV8856_QTECH_ULTRA_SENSOR_ID, 0xA8, Common_read_region},
+	{OV8856_QTECH_FRONT_SENSOR_ID, 0x6C, ov8856_qtech_front_read_otp_info},
+	{GC5035_OFILM_SENSOR_ID, 0xA4, Common_read_region},
+	{GC5035_QTECH_SENSOR_ID, 0xA4, Common_read_region},
+#endif
 	{IMX519_SENSOR_ID, 0xA0, Common_read_region},
 	{S5K2T7SP_SENSOR_ID, 0xA4, Common_read_region},
 	{IMX338_SENSOR_ID, 0xA0, Common_read_region},
@@ -34,6 +83,38 @@ struct stCAM_CAL_LIST_STRUCT g_camCalList[] = {
 	{IMX386_MONO_SENSOR_ID, 0xA0, Common_read_region},
 	/*B+B. No Cal data for main2 OV8856*/
 	{S5K2P7_SENSOR_ID, 0xA0, Common_read_region},
+#ifdef CONFIG_TARGET_PRODUCT_MERLINCOMMON
+	{S5KGM1SP_SENSOR_ID, 0xA0, Common_read_region},
+	{S5KGM1SP_SUNNY_SENSOR_ID, 0xA0, Common_read_region},
+	{OV13B10_SENSOR_ID, 0xA2, Common_read_region},
+	{OV13B10_SUNNY_SENSOR_ID, 0xA2, Common_read_region},
+	{S5K4H7YX_SENSOR_ID, 0xA2, Common_read_region},
+	{S5K4H7YX_SUNNY_SENSOR_ID, 0xA2, Common_read_region},
+	{OV2680_SUNNY_SENSOR_ID, 0xA0, Common_read_region},
+	{OV2180_SENSOR_ID, 0xA4, Common_read_region},
+	{OV2180_SUNNY_SENSOR_ID, 0xA4, Common_read_region},
+	{GC02M1_MACRO_SENSOR_ID, 0xA0, Common_read_region},
+	{GC02M1_MACRO_SUNNY_SENSOR_ID, 0xA0, Common_read_region},
+	{S5KGM1SP_SENSOR_INDIA_ID, 0xA0, Common_read_region},
+	{S5KGM1SP_SUNNY_SENSOR_INDIA_ID, 0xA0, Common_read_region},
+	{OV13B10_SENSOR_INDIA_ID, 0xA2, Common_read_region},
+	{OV13B10_SUNNY_SENSOR_INDIA_ID, 0xA2, Common_read_region},
+	{S5K4H7YX_SENSOR_INDIA_ID, 0xA2, Common_read_region},
+	{S5K4H7YX_SUNNY_SENSOR_INDIA_ID, 0xA2, Common_read_region},
+	{OV2680_SUNNY_SENSOR_INDIA_ID, 0xA0, Common_read_region},
+	{OV2180_SENSOR_INDIA_ID, 0xA4, Common_read_region},
+	{OV2180_SUNNY_SENSOR_INDIA_ID, 0xA4, Common_read_region},
+	{GC02M1_MACRO_SENSOR_INDIA_ID, 0xA0, Common_read_region},
+	{GC02M1_MACRO_SUNNY_SENSOR_INDIA_ID, 0xA0, Common_read_region},
+#ifdef GC02M1_MIPI_RAW
+	{GC02M1_SENSOR_ID, 0x6E, gc02m1_read_otp_info},
+	{GC02M1_SENSOR_INDIA_ID, 0x6E, gc02m1_read_otp_info},
+#endif
+#ifdef GC02M1_SUNNY_MIPI_RAW
+	{GC02M1_SUNNY_SENSOR_ID, 0x6E, gc02m1_sunny_read_otp_info},
+	{GC02M1_SUNNY_SENSOR_INDIA_ID, 0x6E, gc02m1_sunny_read_otp_info},
+#endif
+#endif
 #ifdef SUPPORT_S5K4H7
 	{S5K4H7_SENSOR_ID, 0xA0, zte_s5k4h7_read_region},
 	{S5K4H7SUB_SENSOR_ID, 0xA0, zte_s5k4h7_sub_read_region},

MrShockWAVEog/android_kernel_xiaomi_mt6768-k419

Change-Id: I10a9cc3c6fc6bb25b60aeb850d50ed1dc68806a8

drivers/misc/mediatek/cam_cal/src/mt6768/cam_cal_list.c
@@ -8,9 +8,59 @@
 #include "eeprom_i2c_common_driver.h"
 #include "eeprom_i2c_custom_driver.h"
 #include "kd_imgsensor.h"
+#ifdef GC02M1_MIPI_RAW
+//extern gc02m1_read_otp_info();
+extern unsigned int gc02m1_read_otp_info(struct i2c_client *client,
+	unsigned int addr,
+	unsigned char *data,
+	unsigned int size);
+#endif
+#ifdef GC02M1_SUNNY_MIPI_RAW
+extern unsigned int gc02m1_sunny_read_otp_info(struct i2c_client *client,
+	unsigned int addr,
+	unsigned char *data,
+	unsigned int size);
+#endif
+#ifdef CONFIG_TARGET_PRODUCT_LANCELOT
+#if defined(OV8856_QTECH_FRONT_MIPI_RAW)
+extern unsigned int ov8856_qtech_front_read_otp_info(struct i2c_client *client, unsigned int addr, unsigned char *data, unsigned int size);
+#endif
+#endif
+
+#ifdef CONFIG_TARGET_PRODUCT_SHIVA
+#if defined(OV8856_QTECH_FRONT_MIPI_RAW)
+extern unsigned int ov8856_qtech_front_read_otp_info(struct i2c_client *client, unsigned int addr, unsigned char *data, unsigned int size);
+#endif
+#endif
 
 struct stCAM_CAL_LIST_STRUCT g_camCalList[] = {
 	/*Below is commom sensor */
+#ifdef CONFIG_TARGET_PRODUCT_LANCELOT
+	{OV13B10_OFILM_SENSOR_ID, 0xA2, Common_read_region},
+	{OV13B10_QTECH_SENSOR_ID, 0xA2, Common_read_region},
+	{S5K3L6_QTECH_SENSOR_ID, 0xA2, Common_read_region},
+	{S5K4H7YX_OFILM_FRONT_SENSOR_ID, 0xA0, Common_read_region},
+	{S5K4H7YX_OFILM_ULTRA_SENSOR_ID, 0xA8, Common_read_region},
+	{S5K4H7YX_QTECH_FRONT_SENSOR_ID, 0x20, Common_read_region},
+	{S5K4H7YX_QTECH_ULTRA_SENSOR_ID, 0x5A, Common_read_region},
+	{OV8856_QTECH_ULTRA_SENSOR_ID, 0xA8, Common_read_region},
+	{OV8856_QTECH_FRONT_SENSOR_ID, 0x6C, ov8856_qtech_front_read_otp_info},
+	{GC5035_OFILM_SENSOR_ID, 0xA4, Common_read_region},
+	{GC5035_QTECH_SENSOR_ID, 0xA4, Common_read_region},
+#endif
+#ifdef CONFIG_TARGET_PRODUCT_SHIVA
+	{OV13B10_OFILM_SENSOR_ID, 0xA2, Common_read_region},
+	{OV13B10_QTECH_SENSOR_ID, 0xA2, Common_read_region},
+	{S5K3L6_QTECH_SENSOR_ID, 0xA2, Common_read_region},
+	{S5K4H7YX_OFILM_FRONT_SENSOR_ID, 0xA0, Common_read_region},
+	{S5K4H7YX_OFILM_ULTRA_SENSOR_ID, 0xA8, Common_read_region},
+	{S5K4H7YX_QTECH_FRONT_SENSOR_ID, 0x20, Common_read_region},
+	{S5K4H7YX_QTECH_ULTRA_SENSOR_ID, 0x5A, Common_read_region},
+	{OV8856_QTECH_ULTRA_SENSOR_ID, 0xA8, Common_read_region},
+	{OV8856_QTECH_FRONT_SENSOR_ID, 0x6C, ov8856_qtech_front_read_otp_info},
+	{GC5035_OFILM_SENSOR_ID, 0xA4, Common_read_region},
+	{GC5035_QTECH_SENSOR_ID, 0xA4, Common_read_region},
+#endif
 	{IMX519_SENSOR_ID, 0xA0, Common_read_region},
 	{S5K2T7SP_SENSOR_ID, 0xA4, Common_read_region},
 	{IMX338_SENSOR_ID, 0xA0, Common_read_region},
@@ -24,6 +74,38 @@ struct stCAM_CAL_LIST_STRUCT g_camCalList[] = {
 	{IMX386_MONO_SENSOR_ID, 0xA0, Common_read_region},
 	/*B+B. No Cal data for main2 OV8856*/
 	{S5K2P7_SENSOR_ID, 0xA0, Common_read_region},
+#ifdef CONFIG_TARGET_PRODUCT_MERLIN
+	{S5KGM1SP_SENSOR_ID, 0xA0, Common_read_region},
+	{S5KGM1SP_SUNNY_SENSOR_ID, 0xA0, Common_read_region},
+	{OV13B10_SENSOR_ID, 0xA2, Common_read_region},
+	{OV13B10_SUNNY_SENSOR_ID, 0xA2, Common_read_region},
+	{S5K4H7YX_SENSOR_ID, 0xA2, Common_read_region},
+	{S5K4H7YX_SUNNY_SENSOR_ID, 0xA2, Common_read_region},
+	{OV2680_SUNNY_SENSOR_ID, 0xA0, Common_read_region},
+	{OV2180_SENSOR_ID, 0xA4, Common_read_region},
+	{OV2180_SUNNY_SENSOR_ID, 0xA4, Common_read_region},
+	{GC02M1_MACRO_SENSOR_ID, 0xA0, Common_read_region},
+	{GC02M1_MACRO_SUNNY_SENSOR_ID, 0xA0, Common_read_region},
+	{S5KGM1SP_SENSOR_INDIA_ID, 0xA0, Common_read_region},
+	{S5KGM1SP_SUNNY_SENSOR_INDIA_ID, 0xA0, Common_read_region},
+	{OV13B10_SENSOR_INDIA_ID, 0xA2, Common_read_region},
+	{OV13B10_SUNNY_SENSOR_INDIA_ID, 0xA2, Common_read_region},
+	{S5K4H7YX_SENSOR_INDIA_ID, 0xA2, Common_read_region},
+	{S5K4H7YX_SUNNY_SENSOR_INDIA_ID, 0xA2, Common_read_region},
+	{OV2680_SUNNY_SENSOR_INDIA_ID, 0xA0, Common_read_region},
+	{OV2180_SENSOR_INDIA_ID, 0xA4, Common_read_region},
+	{OV2180_SUNNY_SENSOR_INDIA_ID, 0xA4, Common_read_region},
+	{GC02M1_MACRO_SENSOR_INDIA_ID, 0xA0, Common_read_region},
+	{GC02M1_MACRO_SUNNY_SENSOR_INDIA_ID, 0xA0, Common_read_region},
+#ifdef GC02M1_MIPI_RAW
+	{GC02M1_SENSOR_ID, 0x6E, gc02m1_read_otp_info},
+	{GC02M1_SENSOR_INDIA_ID, 0x6E, gc02m1_read_otp_info},
+#endif
+#ifdef GC02M1_SUNNY_MIPI_RAW
+	{GC02M1_SUNNY_SENSOR_ID, 0x6E, gc02m1_sunny_read_otp_info},
+	{GC02M1_SUNNY_SENSOR_INDIA_ID, 0x6E, gc02m1_sunny_read_otp_info},
+#endif
+#endif
 #ifdef SUPPORT_S5K4H7
 	{S5K4H7_SENSOR_ID, 0xA0, zte_s5k4h7_read_region},
 	{S5K4H7SUB_SENSOR_ID, 0xA0, zte_s5k4h7_sub_read_region},